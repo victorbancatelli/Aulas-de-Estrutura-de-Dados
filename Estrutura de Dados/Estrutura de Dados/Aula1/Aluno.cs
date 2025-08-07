c0418437996cc0f37e131bbb2ce9e59a8587f623
victorbancatelli <victorbancatelli@gmail.com>
2025-08-07 19:55:17 -0300

    feat(Aula1): adicionar arquivos aluno.cs e Exercício.cs

---
+++ Estrutura de Dados/Estrutura de Dados/Aula1/Aluno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estrutura_de_Dados.Aula1
+{
+    internal class Aluno
+    {
+
+        string RA;
+        string Nome;
+        string Endereco;
+        int IdadeIngresso;
+        bool Bolsista;
+
+    }
+}
---
+++ Estrutura de Dados/Estrutura de Dados/Aula1/Exercício.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Double NI, PI, PO, NF;
+
+//inicio_algoritmo
+Console.WriteLine("Insira a sua nota da NI: ");
+NI = float.Parse(Console.ReadLine());
+Console.WriteLine("Insira a sua nota da PI: ");
+PI = float.Parse(Console.ReadLine());
+Console.WriteLine("Insira a sua nota da PO: ");
+PO = float.Parse(Console.ReadLine());
+
+NF = NI * 0.2 + PI * 0.3 + PO * 0.5;
+
+//fim_algoritmo
+Console.WriteLine("A sua média esse semestre foi igual a " + NF);
